ae90467048f15beee8f0dab4b4a75c03ea772f00
autumnleaf <shaoyanzunjun@qq.com>
2022-12-01 17:18:37 +0800

    Initial commit

---
+++ src/main/java/com/fzy/test/Main.java
@@ -0,0 +1,14 @@
+package com.fzy.test;
+
+/**
+ * @author fzy
+ * @Description
+ * @create 2022-12-01 17:17
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is a test.");
+    }
+}
